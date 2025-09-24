2948d808bd7d61c945b060c6a244eda24e09466d
Bilal Shaikh <bilalshaikhh339@gmail.com>
2025-09-24 07:21:07 -0700

    i

---
+++ app.js
@@ -0,0 +1,76 @@
+         var arr1 = [1,2,3,4,5,6]
+           for(var i = 0;i<arr1.length;i++){
+            console.log(arr1[i])
+           }
+
+
+
+           var arr2 = [1,2,3,4,5,6]
+           for(var i = (arr2.length-1); i>=0  ;i--){
+            console.log(arr2[i])
+
+           }
+
+
+        var arr3 = [1, 2, 3, 4, 5, 6]
+        var sum = 0
+        for (var i = 0; i < arr3.length; i++) {
+            sum = sum + arr3[i]
+            }
+            console.log(sum)
+
+
+
+        var arr3 = [1, 2, 3, 4, 5, 6]
+        var sum = 0
+        for (var i = 0; i < arr3.length; i++) {
+            sum = sum + arr3[i]
+
+
+            }
+           var avg = (sum/arr3.length)
+
+            console.log(avg)
+
+
+        var arr3 = [1, 2, 3, 4, 5, 6];
+        for (var i = 0; i < arr3.length; i++) {
+            if (arr3[i] % 2 === 0) {
+                console.log(arr3[i])
+            }
+        }
+
+
+
+        var arr3 = [1, 2, 3, 4, 5, 6];
+        for (var i = 0; i < arr3.length; i++) {
+            if (arr3[i] % 2 === 1) {
+                console.log(arr3[i])
+            }
+        }
+
+
+           var arr = [1, 2, 3, 4, 5, 6];
+           for(var i= 0 ; i<arr.length; i++){
+            console.log(arr[i]*2)
+           }
+
+           var arr = [1, 2, 3, 4, 5, 6];
+           for(var i= 0 ; i<arr.length; i++){
+            console.log(arr[i] * arr[i])
+        }
+
+
+        var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+           for(var i= 0 ; i<arr.length; i++){
+            if(arr[i] > 5 ){
+                console.log(arr[i])
+            }
+        }
+
+
+
+
+
+
+
